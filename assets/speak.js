62e1db9c06dde5f2e010f27af23a4fdd458fd597
Tobias Merkle <tobias@branchmessenger.com>
2022-10-08 20:42:11 -0400

    switch to miguel

--- assets/speak.js
+++ assets/speak.js
@@ -46,7 +46,7 @@ var VOICES = {
     Filiz: "Filiz [Turkish]",
     Gwyneth: "Gwyneth [Welsh]",
 };
-var DEFAULT_VOICE = 'Joey';
+var DEFAULT_VOICE = 'Miguel';
 
 $(document).ready(function() {
     $('.speak').on('click', function(e) {
